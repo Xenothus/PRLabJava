da16af4f183901212cb180fffccd873be249b438
Xenothus <lukasz.slazyk@gmail.com>
2017-05-22 17:10:15 +0200

    Changed Well - inheritance SimpleWorkplace

--- src/Places/Well.java
+++ src/Places/Well.java
@@ -1,61 +1,28 @@
 package Places;
 
-public class Well {
+public class Well extends SimpleWorkplace {
 
-    private static int waterStorage = 0;
-    private static int lastWaterDemand = 0;
-
-    private static final Object lastWaterDemandLock = new Object();
     private static class SingletonHelper {
         private static final Well instance = new Well();
     }
 
-    private Well(){}
+    private Well()
+    {
+        super.init();
+    }
 
     public static Well getInstance()
     {
         return SingletonHelper.instance;
     }
 
-    public synchronized int takeWater(int tons)
+    public int takeWater(int units)
     {
-        try
-        {
-            if (tons > waterStorage)
-            {
-                setLastWaterDemand(tons);
-                wait();
-            }
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-            return 0;
-        }
-
-        waterStorage -= tons;
-        return tons;
+        return super.takeProduct(units);
     }
 
-    public synchronized void putWater(int tons)
+    public void putWater(int units)
     {
-        waterStorage += tons;
-        if (waterStorage >= getLastWaterDemand())
-            notify();
-    }
-
-    private int getLastWaterDemand()
-    {
-        synchronized (lastWaterDemandLock)
-        {
-            return lastWaterDemand;
-        }
-    }
-
-    private void setLastWaterDemand(int input)
-    {
-        synchronized (lastWaterDemandLock)
-        {
-            Well.lastWaterDemand = input;
-        }
+        super.putProduct(units);
     }
 }
